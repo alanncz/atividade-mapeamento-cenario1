faf22ffd321b95ec9a881e2f71230373c9aa8f34
Alann Rodrigues Ferreira <alannrodrigues@hotmail.com>
2017-10-19 00:15:38 -0300

    Subindo aplicação finalizada

--- target/generated-sources/annotations/com/atividade/dac/cenario1/Conta_.java
+++ target/generated-sources/annotations/com/atividade/dac/cenario1/Conta_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-18T22:23:56")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-18T23:52:22")
 @StaticMetamodel(Conta.class)
 public abstract class Conta_ { 
 
--- target/generated-sources/annotations/com/atividade/dac/cenario1/Transacao_.java
+++ target/generated-sources/annotations/com/atividade/dac/cenario1/Transacao_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-18T22:23:56")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-18T23:52:22")
 @StaticMetamodel(Transacao.class)
 public class Transacao_ { 
 
